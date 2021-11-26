b698e6350ecf87e14174e5d7e5d1d95336c1a5f3
Genrikh Fetischev <genrikhfetischev@gmail.com>
2021-11-26 13:33:48 +0300

    refactoring & fixes

--- src/index.ts
+++ src/index.ts
@@ -2,55 +2,78 @@ import { resolve } from "path";
 import { readFileSync, writeFileSync } from "fs";
 import { execShellCommand } from "./execShellCommand";
 
-(async () => {
-  const tryToCallGitVersion = await execShellCommand("gitversion");
+const execGitVersion = async (): Promise<string> => {
+  const commandOutput = await execShellCommand("gitversion");
 
-  if (tryToCallGitVersion.exitCode !== 0) {
-    console.log(
-      "\x1b[31m",
-      `Error, 'gitversion' command exited with code ${tryToCallGitVersion.exitCode}`
+  if (commandOutput.exitCode !== 0) {
+    throw Error(
+      `Error, 'gitversion' command exited with code ${commandOutput.exitCode}`
     );
-    return;
   }
 
-  let gitversionOut: { [key: string]: string };
-  let packageJsonContent: { [key: string]: string };
+  return commandOutput.stdout.join("");
+};
 
-  let fullSemVer: string = "";
-
-  const pathToPackageJson = resolve("./package.json");
-
+const tryToParseGitversionStdout = (
+  stdout: string
+): Record<string, string> | null => {
   try {
-    packageJsonContent = JSON.parse(
-      readFileSync(pathToPackageJson, { encoding: "utf8" })
-    );
+    return JSON.parse(stdout);
+  } catch (e) {
+    console.error("\x1b[31m", "Error while parsing gitversion result");
+    console.error(e);
+    return null;
+  }
+};
+
+const tryToGetVersionViaRegexp = (stdout: string): string | null => {
+  const match = stdout.match(/FullSemVer.+(\d+\.\d+\.\d+)(.+)?"/);
+  const result = `${match?.[1] === undefined ? "" : match?.[1]}${
+    match?.[2] === undefined ? "" : match?.[2]
+  }`;
+  return result === "" ? null : result;
+};
+
+const readPackageJson = (
+  pathToPackageJson: string
+): Record<string, string> => {
+  try {
+    return JSON.parse(readFileSync(pathToPackageJson, { encoding: "utf8" }));
   } catch (e) {
     console.error("\x1b[31m", "Error while reading package.json");
     throw e;
   }
+};
 
-  try {
-    gitversionOut = JSON.parse(
-      tryToCallGitVersion.stdout.reduce((acc, item) => acc + item)
-    );
-    fullSemVer = gitversionOut.FullSemVer;
-  } catch (e) {
-    console.error("\x1b[31m", "Error while parsing gitversion result");
-  }
-
-  console.log("Try to get FullSemVer via regexp");
-  const match = tryToCallGitVersion.stdout
-    .join("")
-    .match(/FullSemVer.+(\d+\.\d+\.\d+)/);
-
-  fullSemVer = match[1] ? match[1] : undefined;
-
-  if (!fullSemVer) {
-    throw Error("can't parse full semver via regexp");
-  }
-
-  packageJsonContent.version = fullSemVer;
-
+const patchPackageJson = (
+  version: string,
+  packageJsonContent: Record<string, string>,
+  pathToPackageJson: string
+) => {
+  packageJsonContent.version = version;
   writeFileSync(pathToPackageJson, JSON.stringify(packageJsonContent, null, 2));
+};
+
+(async () => {
+  const gitversionOut = await execGitVersion();
+  if (!gitversionOut) {
+    return;
+  }
+
+  const fullSemver =
+    tryToParseGitversionStdout(gitversionOut)?.FullSemVer ??
+    tryToGetVersionViaRegexp(gitversionOut);
+
+  if (!fullSemver) {
+    throw Error("Couldn't get full semver");
+  }
+
+  const pathToPackageJson = resolve("./package.json");
+
+  const packageJsonContent = readPackageJson(pathToPackageJson);
+
+  patchPackageJson(fullSemver, packageJsonContent, pathToPackageJson);
+
   console.log("\x1b[32m", "Done! 🎾");
 })();
+
